046a49f486f6b8be6f8e250299daef850f4200ff
Stas <smaster950@gmail.com>
2022-12-13 20:10:24 +0300

    Complete Exercise_2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,2 +1,14 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 # not (X or Y or Z) = not X and not Y and not Z
+
+x = bool(input('Input True or False: '))
+y = bool(input('Input True or False: '))
+z = bool(input('Input True or False: '))
+
+result1 = not (x or y or z)
+result2 = (not x) and (not y) and (not z)
+
+if result1 == result2:
+    print('True')
+else:
+    print('False')
